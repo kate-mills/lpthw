486913a09a8d73f263af861e5e1908255ff362f4
Kate Mills <kate.mills1124@gmail.com>
2017-01-12 13:12:34 -0800

    Added Math Symbols

---
+++ python/ex3.py
@@ -0,0 +1,19 @@
+print "\nMath Symbols - PEMDAS"
+print "What is 7.0 / 4.0\t",7.0/4.0
+print "What is 7 / 4\t\t",7/4
+
+print "\nI will now count cups of Hot Chocolate:"
+print "Swiss Miss\t",25 + 30 / 6
+print "Carnation\t",100 - 25 * 3 % 4
+print "\nNow I will count marshmallows:"
+print "\t\t",3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "\nDo I have more Swiss Miss or Carnation?"
+print "Swiss Miss:\t",3 + 2
+print "Carnation:\t",7 - 5
+print "\nI have more Swiss Miss:",3 + 2 > 5 - 7
+print "What is 3 + 2?\t",3 + 2
+print "What is 5 -7?\t",5 - 7
+print "\nOh, how I love hot chocolate!"
+print "Is it greater?\t\t", 5 > -2
+print "Is is greater or equal?\t",5 >= -2
+print "Is is less or equal?\t",5 <= -2
