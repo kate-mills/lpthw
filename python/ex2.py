16a8b579081ab9964087e2530e2389e7e67b3a9c
Kate Mills <kate.mills1124@gmail.com>
2017-01-11 18:42:03 -0800

    Added comments

---
+++ python/ex2.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#   octothorpe
+print "octothorpe = #"
+print "# -*- coding: utf-8 -*-"
+print"a little workaround for problems with settings and detecting the format of a file"
