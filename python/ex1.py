5008aa7d61ee7605b05e42fc54275968fd54a1b9
Kate Mills <kate.mills1124@gmail.com>
2017-01-12 11:33:29 -0800

    Updated atom and terminal shortcuts

--- python/ex1.py
+++ python/ex1.py
@@ -1,5 +1,16 @@
 print "\n"
-print "My Favorite Atom Short Cuts"
-print "Command Pallete: # + Shift + P"
-print "Quit: # + Q"
-print "Minimize: # + M"
+print "\tMy Favorite Atom Short Cuts\n"
+print "Cmd pallete:\r\t\t# + Shift + P"
+print "Quit:\r\t\t# + Q"
+print "Minimize:\r\t\t# + M"
+print "Hide App:\r\t\t# + H"
+
+
+
+print "\n\tTerminal Commands\n"
+print "xargs:\texecute arguments"
+print "find:\tfind files"
+print "grep:\tfind things inside files"
+print "man:\tread a manual page"
+print "env:\tlook at your environment"
+print "apropos:\n\tfind what man page is appropriate"
