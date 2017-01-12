6968396418e17b440b36787d9d390cfb0a5e482f
Kate Mills <kate.mills1124@gmail.com>
2017-01-12 11:49:21 -0800

    updated Terminal Commands and moved apropos to top of list

--- python/ex1.py
+++ python/ex1.py
@@ -8,9 +8,12 @@ print "Hide App:\r\t\t# + H"
 
 
 print "\n\tTerminal Commands\n"
+print "apropos:\n\tfind what man page is appropriate"
 print "xargs:\texecute arguments"
 print "find:\tfind files"
 print "grep:\tfind things inside files"
 print "man:\tread a manual page"
 print "env:\tlook at your environment"
-print "apropos:\n\tfind what man page is appropriate"
+print "export:\t export/set a new environment variable"
+print "cat: \tprint the whole file"
+print "less: \tpage through a file"
