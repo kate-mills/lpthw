e8de82c328e27547d10bbf870e38e9fbb3102853
Kate Mills <kate.mills1124@gmail.com>
2017-01-15 11:23:15 -0800

    Importing Modules - from sys import argv

---
+++ python/ex13.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script, first, second, third = argv
+
+
+print """
+\tParameters, Unpacking, Variables
+"""
+
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
