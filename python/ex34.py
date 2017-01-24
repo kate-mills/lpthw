ca59b3ca879e8df62dea982c429cfd16402b7cef
Kate Mills <kate.mills1124@gmail.com>
2017-01-24 14:21:25 -0800

    Reminders

---
+++ python/ex34.py
@@ -0,0 +1,13 @@
+reminders = """
+ex13 - ex 16:
+        from sys import argv
+
+
+ex17:   from sys import argv
+        from os.path import exists
+
+ex20:   from sys import argv
+
+ex35:   from sys import exit
+"""
+print reminders
