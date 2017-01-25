5e38610c0741e07d619352eef838a967e0f8e48d
Kate Mills <kate.mills1124@gmail.com>
2017-01-25 14:02:54 -0800

    Assert examples

---
+++ python/ex36.py
@@ -0,0 +1,17 @@
+i = 20
+
+if i < 20:
+    print "yes"
+elif i >= 20:
+    print "greater or equal"
+else:
+    assert False, "Broken if # 1"
+
+
+
+if i < 20:
+    print "yes"
+elif i > 20:
+    print "no"
+else:
+    assert False, "Broken if # 2."
