ecd9511cfb693557c9c979045faf46cd959f9b82
Kate Mills <kate.mills1124@gmail.com>
2017-01-28 09:31:05 -0800

    Added Dict vs Module vs Class

---
+++ python/ex40.py
@@ -0,0 +1,33 @@
+import mystuff
+
+# dict
+mystuff_dict = {'apple': 'I AM APPLES', 'tangerine': 'Living Reflection'}
+print "dictionary example: "
+print "\t%s %s" % ( mystuff_dict['apple'], mystuff_dict['tangerine']), "\n"
+
+
+# module
+print "module example: "
+print "\t %s %s" % (mystuff.apple(), mystuff.tangerine), "\n"
+
+
+# class
+thing = mystuff.MyStuff()
+print "class example: "
+print "\t", thing.tangerine, thing.apple(), "\n"
+
+
+# class
+happy_bday = mystuff.Song(["Happy birthday to you",
+                "I don't want to get sued",
+                "So I'll stop right there"])
+
+bulls_on_parade = mystuff.Song(["They rally around the family",
+                "With pockets full of shells"])
+
+
+print "another class example: ", "\n"
+
+
+happy_bday.sing_me_a_song(),
+bulls_on_parade.sing_me_a_song()
---
+++ python/mystuff.py
@@ -0,0 +1,26 @@
+
+def apple():
+    return "I AM APPLES!"
+
+tangerine = "Living reflection of a dream."
+
+
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        return "I AM CLASSY APPLES!"
+
+
+
+
+
+class Song(object):
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
