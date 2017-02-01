04653ce21cb0eea9e980383945f4755cf9d00783
Kate Mills <kate.mills1124@gmail.com>
2017-02-01 15:44:30 -0800

    Added example project gesso

---
+++ python/projects/gesso/gesso/oil.py
@@ -0,0 +1,5 @@
+def paint_canvas(color):
+    print color * 10
+
+def paint_board(material, paint):
+    print material, paint
---
+++ python/projects/gesso/tests/gesso_tests.py
@@ -0,0 +1,21 @@
+from nose.tools import *
+from gesso import acrylic
+from gesso import oil
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
+
+def test_paint():
+    acrylic.paint("blue")
+
+def test_paint_canvas():
+    oil.paint_canvas("blue")
+
+def test_paint_board():
+    oil.paint_board("mixed media", "red")
