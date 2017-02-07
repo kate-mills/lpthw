1c636f21924e045f1f161cdece8ee3290b9ba825
Kate Mills <kate.mills1124@gmail.com>
2017-02-07 12:51:01 -0800

    Errors are added to lexicon without changing to upper()

--- python/projects/ex48/ex48/lexicon.py
+++ python/projects/ex48/ex48/lexicon.py
@@ -1,4 +1,3 @@
-
 lexicon = {
     'north': 'direction',
     'south': 'direction',
@@ -18,38 +17,36 @@ lexicon = {
     '1234': 'number',
     '3': 'number',
     '91234': 'number'
-}
 
+}
 
 def convert_number(s):
     try:
         return int(s)
-    except ValueError:
-        return s.upper()
 
+    except ValueError:
+        return s
 
 
 def scan(paragraph):
-    words = paragraph.lower().split()
+    words = paragraph.split()
     result = []
 
     for word in words:
-        #add word to lexicon as an error
+        # if word is not in lexicon, add it.
         if not(word in lexicon):
-            word = word.upper()
             lexicon[word] = 'error'
             pair = (lexicon[word], word)
 
+        # if value in lexicon is not 'number' and value in lexicon is not 'error'
         if (lexicon[word] != 'number' and lexicon[word] != 'error'):
             pair = (lexicon[word], word)
 
+        # word must be a number or error
         else:
-            number_or_error = convert_number(word)
-            pair = (lexicon[word], number_or_error)
+            num_or_err = convert_number(word)
+            pair = (lexicon[word], num_or_err )
 
         result.append(pair)
 
     return result
-
-scanned = scan('IAS 1234 blah raley in migrate South The In ')
-print scanned
