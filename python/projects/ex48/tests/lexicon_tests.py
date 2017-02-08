6d3156054c160ab15b289f0a7f2d5fbcf31744f3
Kate Mills <kate.mills1124@gmail.com>
2017-02-07 17:57:36 -0800

    Changed name and added parser

--- python/projects/ex48/tests/lexicon_tests.py
+++ python/projects/ex48/tests/lexicon_tests.py
@@ -2,6 +2,7 @@ from nose.tools import *
 from ex48 import lexicon
 
 
+
 def test_directions():
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
     result = lexicon.scan("north south east")
---
+++ python/projects/ex48/tests/parser_tests.py
@@ -0,0 +1,27 @@
+from nose.tools import *
+# from ex49.parser import Sentence
+from ex48 import parser
+
+def test_Sentence():
+    pass
+
+def test_peek():
+    pass
+
+def test_match():
+    pass
+
+def test_skip():
+    pass
+
+def test_parse_verb():
+    pass
+
+def test_object():
+    pass
+
+def test_parse_subject():
+    pass
+
+def test_parse_sentence():
+    pass
