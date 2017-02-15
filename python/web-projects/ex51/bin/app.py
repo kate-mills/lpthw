080ceac2e2890708c0100774628e6a05f7e48d95
Kate Mills <kate.mills1124@gmail.com>
2017-02-14 16:56:06 -0800

    First -a Commit

---
+++ python/web-projects/ex51/bin/app.py
@@ -0,0 +1,22 @@
+
+import web
+
+urls = (
+'/hello', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/', base="layout")
+
+class Index(object):
+    def GET(self):
+        return render.hello_form() # going to display hello_form.html instead of index.html
+
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hola") # gets input from the browser and converts it to a variable we can display
+        greeting = " %s, %s " %( form.greet, form.name )
+        return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+    app.run()
